ede8b718dc6cebf6c43fef38938d74c6c40f2451
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-11-06 15:46:36 +0000

    changed lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,8 @@ module.exports = {
     'react/prefer-stateless-function': 'off',
 
     'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
     'jsx-a11y/label-has-for': [
       2,
       {
