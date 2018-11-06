dae10f762a41198f865b2e8e74b41135f7c6d8e9
David Gridley <davidgridley21@gmail.com>
2018-11-06 11:39:16 +0000

    updated database with users

--- database.sql
+++ database.sql
@@ -1,24 +1,30 @@
 CREATE DATABASE opentab;
 
 CREATE TABLE "user" (
-  id SERIAL PRIMARY KEY,
-  username varchar(30) NOT NULL,
-  password text NOT NULL,
-  email varchar(50) UNIQUE,
-  phone varchar(20)
-); 
+id SERIAL PRIMARY KEY,
+username varchar(30) NOT NULL,
+password text NOT NULL,
+email varchar(50) UNIQUE,
+phone varchar(20)
+);
 
 CREATE TABLE round (
-  id SERIAL PRIMARY KEY,
-  buyer_id INT NOT NULL,
-  time TIMESTAMP WITH TIME ZONE NOT NULL,
-  FOREIGN KEY (buyer_id) REFERENCES "user" (id)
+id SERIAL PRIMARY KEY,
+buyer_id INT NOT NULL,
+time TIMESTAMP WITH TIME ZONE NOT NULL,
+FOREIGN KEY (buyer_id) REFERENCES "user" (id)
 );
 
 CREATE TABLE round_user (
-  id SERIAL PRIMARY KEY,
-  round_id INT NOT NULL,
-  recipient_id INT NOT NULL,
-  FOREIGN KEY (recipient_id) REFERENCES "user" (id),
-  FOREIGN KEY (round_id) REFERENCES round (id)
+id SERIAL PRIMARY KEY,
+round_id INT NOT NULL,
+recipient_id INT NOT NULL,
+FOREIGN KEY (recipient_id) REFERENCES "user" (id),
+FOREIGN KEY (round_id) REFERENCES round (id)
 );
+
+INSERT INTO "user" (username, password, email, phone) VALUES ('Yetkin', 'test', 'yetkin@gmail.com', '07998777666');
+INSERT INTO "user" (username, password, email, phone) VALUES ('Dave', 'test', 'dave@gmail.com', '07998555444');
+INSERT INTO "user" (username, password, email, phone) VALUES ('Dan', 'test', 'dan@gmail.com', '07998444333');
+INSERT INTO "user" (username, password, email, phone) VALUES ('Tony', 'test', 'tony@gmail.com', '07998333222');
+INSERT INTO "user" (username, password, email, phone) VALUES ('Luke', 'test', 'luke@gmail.com', '07998222111');
