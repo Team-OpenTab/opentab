d9d2c9d688c7a8b07164f5d38195ba884a28dc18
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-11-07 11:59:52 +0000

    simplified newround

--- src/components/NewRound.js
+++ src/components/NewRound.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import AmountToPayContainer from '../containers/AmountToPayContainer';
-import UserSelectionContainer from '../containers/UserSelectionContainer';
-import SubmitCheckedUsersContainer from '../containers/SubmitCheckedUsersContainer';
+import PropTypes from 'prop-types';
 import '../../styles/components/NewRound.scss';
 
-function NewRound() {
+function NewRound({
+  affectAmountPaid,
+  amount,
+  monitorCheckedUser,
+  submitCheckedUsers,
+  checkedUsers,
+}) {
   return (
     <main>
-      <AmountToPayContainer />
+      <input value={amount} onChange={event => affectAmountPaid(event.target.value)} />;
       <section>
-        <UserSelectionContainer />
+        <ul className="user__selection">
+          <li>
+            <input onChange={() => monitorCheckedUser('Tony')} type="checkbox" />
+            Tony
+          </li>
+          <li>
+            <input onChange={() => monitorCheckedUser('Luke')} type="checkbox" />
+            Luke
+          </li>
+          <li>
+            <input onChange={() => monitorCheckedUser('Dan')} type="checkbox" />
+            Dan
+          </li>
+          <li>
+            <input onChange={() => monitorCheckedUser('Yetkin')} type="checkbox" />
+            Yetkin
+          </li>
+          <li>
+            <input onChange={() => monitorCheckedUser('David')} type="checkbox" />
+            David
+          </li>
+        </ul>
       </section>
-      <SubmitCheckedUsersContainer />
+      <button type="button" onClick={submitCheckedUsers(checkedUsers, amount)}>
+        Submit
+      </button>
     </main>
   );
 }
+NewRound.propTypes = {
+  affectAmountPaid: PropTypes.func,
+  amount: PropTypes.number,
+  monitorCheckedUser: PropTypes.func,
+  submitCheckedUsers: PropTypes.func,
+  checkedUsers: PropTypes.array,
+};
 
 export default NewRound;
