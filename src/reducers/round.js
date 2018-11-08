e3ea5190221b9d58c40a70035a6998180fdb5667
danGRT <d.geraghty06@gmail.com>
2018-11-08 12:24:59 +0000

    can add/remove from round

--- src/reducers/round.js
+++ src/reducers/round.js
@@ -1,13 +1,18 @@
-const initialState = { userId: 1, counterpartIds: [1, 2, 3, 4, 5], totalAmount: 0 };
+const initialState = { userId: 1, counterpartIds: [], totalAmount: 0 };
 function round(state = initialState, action) {
   switch (action.type) {
     case 'ADD_CHECKED_USER':
-      return Object.assign({}, state, { counterpartIds: state.checkedUsers.concat(action.user) });
+      return Object.assign({}, state, {
+        counterpartIds: state.counterpartIds.concat(action.counterpart),
+      });
     case 'REMOVE_CHECKED_USER':
       return Object.assign({}, state, {
-        checkedUsers: state.counterpartIds.filter(user => user !== action.user),
+        counterpartIds: state.counterpartIds.filter(
+          counterpart => counterpart !== action.counterpart,
+        ),
       });
-
+    case 'RESET_ROUND':
+      return Object.assign({}, initialState);
     case 'STORE_USERS_FINAL':
       // probably some socket code gets invoked instead
       return state;
