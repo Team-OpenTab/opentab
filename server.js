78b1740d4c7eaf0fe54f207582d2319786787fb0
David Gridley <davidgridley21@gmail.com>
2018-11-09 16:15:42 +0000

    added get rounds endpoint, returning rounds bought by user

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.post('/api/new-round', (req, res) => {
             ),
             db.one(
               `INSERT INTO transaction (user_id, counterpart_id, round_id, amount, type, time)
-                VALUES ($1, $2, $3, $4, 'round', now())
+                VALUES ($1, $2, $3, $4, $5, now())
                 RETURNING id`,
-              [counterpartId, userId, data.id, amount],
+              [counterpartId, userId, data.id, amount, userId === counterpartId ? 'self' : 'round'],
             ),
             db.none(
               `INSERT INTO round_user (round_id, counterpart_id) 
@@ -161,6 +161,49 @@ app.post('/api/make-payment', (req, res) => {
     .catch(error => console.log(error));
 });
 
+app.get('/api/get-rounds/:userId', (req, res) => {
+  const userId = parseInt(req.params.userId);
+  db.any(
+    `SELECT transaction.round_id, round.user_id, 
+    transaction.counterpart_id, transaction.time, transaction.amount
+    FROM transaction, round 
+    WHERE round.user_id = $1
+    AND round.user_id = transaction.user_id
+    AND type = 'round'
+    AND transaction.amount < 0
+    GROUP BY transaction.round_id, round.user_id, 
+    transaction.counterpart_id, transaction.amount, transaction.time`,
+    [userId],
+  ).then(response => {
+    const groupedRounds = response.reduce((acc, curr) => {
+      if (!acc[curr.round_id]) {
+        acc[curr.round_id] = [];
+      }
+      acc[curr.round_id].push(curr);
+      return acc;
+    }, {});
+    const refinedRoundObjects = Object.keys(groupedRounds).map(key =>
+      groupedRounds[key].reduce((acc, curr) => {
+        const counterparts = !acc.counterparts ? { [curr.user_id]: curr.amount } : acc.counterparts;
+        acc = {
+          roundId: key,
+          userId: curr.user_id,
+          counterparts: Object.assign({}, counterparts, { [curr.counterpart_id]: curr.amount }),
+          roundTotal: Object.values(counterparts).reduce(
+            (agg, val) => parseInt(agg) + parseInt(val),
+            Object.values(counterparts)[0],
+          ),
+          roundTime: curr.time,
+        };
+        return acc;
+      }, {}),
+    );
+    res.json(refinedRoundObjects);
+  });
+});
+
 server.listen(8080, () => {
   console.log('Listening on port 8080');
 });
+
+// Gets round buyer, counterparts, amount given to each counterpart
