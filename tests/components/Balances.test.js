3a1764c5ad171af204ded8c35fa867b0cfaf41c0
danGRT <d.geraghty06@gmail.com>
2018-11-11 16:34:02 +0000

    balances component has tests for event handlers, ensures counterparts are rendered post fetch

--- tests/components/Balances.test.js
+++ tests/components/Balances.test.js
@@ -25,8 +25,25 @@ function setup() {
 }
 
 describe('balances component', () => {
+  const { wrapper, props } = setup();
   it('calls fetchBalances on mount', () => {
-    const { props } = setup();
     expect(props.fetchBalances.mock.calls).toHaveLength(1);
+  });
+  it('renders counterparts after fetch', () => {
+    wrapper.setProps({
+      balances: {
+        userBalance: 0,
+        counterpartBalances: { 1: { username: 'Test', counterpart_id: 1, sum: '-10.00' } },
+      },
+    });
+    expect(wrapper.find('.counterpart').exists()).toBe(true);
+  });
+  it('calls showPayment modal if pay button is clicked', () => {
+    wrapper.find('.counterpart__btn').simulate('click');
+    expect(props.showPayment).toHaveBeenCalled();
+  });
+  it('calls getStage with newRound when new round button is clicked', () => {
+    wrapper.find('.new-round-btn').simulate('click');
+    expect(props.getStage).toHaveBeenCalledWith('newRound');
   });
 });
