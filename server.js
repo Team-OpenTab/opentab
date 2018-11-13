bbd766a9a61093aa8f4b56c1a10070d2971f7f6b
danGRT <d.geraghty06@gmail.com>
2018-11-13 10:27:06 +0000

    round history is placed in store

--- server.js
+++ server.js
@@ -291,17 +291,14 @@ app.post('/api/make-payment', (req, res) => {
 
 app.get('/api/get-rounds/:userId', (req, res) => {
   const { userId } = req.params;
-  console.log(userId);
   db.any('SELECT round_id FROM round_user WHERE counterpart_id = $1', [userId])
     .then(response => {
-      console.log(response);
       const promisesArray = response.map(round =>
         db.any(`SELECT * FROM transaction WHERE round_id = ${round.round_id} AND amount < 0`),
       );
       return Promise.all(promisesArray);
     })
     .then(response => {
-      console.log(response);
       const roundStore = response
         .map(round =>
           round.reduce((acc, curr) => {
@@ -313,8 +310,8 @@ app.get('/api/get-rounds/:userId', (req, res) => {
               userId: curr.user_id,
               counterparts: Object.assign({}, counterparts, { [curr.counterpart_id]: curr.amount }),
               roundTotal: Object.values(counterparts).reduce(
-                (agg, val) => parseInt(agg) + parseInt(val),
-                Object.values(counterparts)[0],
+                (agg, val) => parseFloat(agg) + parseFloat(val),
+                0,
               ),
               roundTime: curr.time,
             };
@@ -322,7 +319,6 @@ app.get('/api/get-rounds/:userId', (req, res) => {
           }, {}),
         )
         .filter(round => round.hasOwnProperty('roundId'));
-      console.log(roundStore);
       res.json(roundStore);
     });
 });
