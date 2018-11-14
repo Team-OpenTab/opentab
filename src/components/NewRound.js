3af9f8fb497b0aa8d831e107200db63cb7b7ff09
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-11-14 11:13:22 +0000

    fixed rounding bug in new round

--- src/components/NewRound.js
+++ src/components/NewRound.js
@@ -23,13 +23,13 @@ function NewRound({
       <TitleBar title="New Round" previous="balances" getStage={getStage} stage={stage} />
       <div className="new-round__amount">
         <h3>Round Amount</h3>
-        <input value={totalAmount} onChange={event => getAmount(event.target.value)} />
+        <input value={totalAmount} onChange={(event) => getAmount(event.target.value)} />
         <input
           type="radio"
           name="splitType"
           id="radio1"
           value="even"
-          onClick={event => getSplitType(event.target.value)}
+          onClick={(event) => getSplitType(event.target.value)}
           defaultChecked
         />
         <label htmlFor="radio1">Split Evenly</label>
@@ -38,7 +38,7 @@ function NewRound({
           name="splitType"
           id="radio2"
           value="manual"
-          onClick={event => getSplitType(event.target.value)}
+          onClick={(event) => getSplitType(event.target.value)}
         />
         <label htmlFor="radio2">Split Manually</label>
       </div>
@@ -57,17 +57,17 @@ function NewRound({
             (splitType === 'manual' ? (
               <input
                 className="new-round__input"
-                value={recipients[userId].toFixed(2)}
-                onChange={event => getRecipientAmount(userId, event.target.value)}
+                value={recipients[userId]}
+                onChange={(event) => getRecipientAmount(userId, event.target.value)}
               />
             ) : (
-              <p className="new-round__even-amount">{recipients[userId].toFixed(2)}</p>
+              <p className="new-round__even-amount">{recipients[userId]}</p>
             ))}
         </div>
         {/* RECIPIENTS */}
         {Object.keys(recipients)
-          .filter(recipient => Number(recipient) !== userId)
-          .map(recipient => (
+          .filter((recipient) => Number(recipient) !== userId)
+          .map((recipient) => (
             <div className="new-round__counterpart added-recipient" key={recipient}>
               <h3 className="new-round__counterpart-name">{counterparts[recipient].username}</h3>
               <button
@@ -81,16 +81,16 @@ function NewRound({
               {splitType === 'manual' ? (
                 <input
                   className="new-round__input"
-                  value={recipients[recipient].toFixed(2)}
-                  onChange={event => getRecipientAmount(recipient, event.target.value)}
+                  value={recipients[recipient]}
+                  onChange={(event) => getRecipientAmount(recipient, event.target.value)}
                 />
               ) : (
-                <p className="new-round__even-amount">{recipients[recipient].toFixed(2)}</p>
+                <p className="new-round__even-amount">{recipients[recipient]}</p>
               )}
             </div>
           ))}
 
-        {Object.values(counterparts).map(counterpart => (
+        {Object.values(counterparts).map((counterpart) => (
           <div key={counterpart.counterpart_id}>
             {!Object.keys(recipients).includes(counterpart.counterpart_id.toString()) && (
               <div className="new-round__counterpart">
