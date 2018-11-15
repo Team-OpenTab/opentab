c243322a64be4ca2ecd65ea37df480ab1486ddbf
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-11-15 20:07:37 +0000

    email to lower case

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,14 +44,14 @@ function Login({
               className="form__field"
               type="text"
               value={email}
-              onChange={event => getEmail(event.target.value)}
+              onChange={(event) => getEmail(event.target.value.toLowerCase())}
               placeholder="Email"
             />
             <input
               className="form__field"
               type="password"
               value={password}
-              onChange={event => getPassword(event.target.value)}
+              onChange={(event) => getPassword(event.target.value)}
               placeholder="Password"
             />
             <div>{loginError}</div>
@@ -83,42 +83,42 @@ function Login({
               className="form__field"
               type="text"
               value={email}
-              onChange={event => getEmail(event.target.value)}
+              onChange={(event) => getEmail(event.target.value)}
               placeholder="Email*"
             />
             <input
               className="form__field"
               type="text"
               value={username}
-              onChange={event => getUsername(event.target.value)}
+              onChange={(event) => getUsername(event.target.value)}
               placeholder="Username*"
             />
             <input
               className="form__field"
               type="text"
               value={phone}
-              onChange={event => getPhone(event.target.value)}
+              onChange={(event) => getPhone(event.target.value)}
               placeholder="Phone"
             />
             <input
               className="form__field"
               type="text"
               value={avatar}
-              onChange={event => getAvatar(event.target.value)}
+              onChange={(event) => getAvatar(event.target.value)}
               placeholder="Avatar URL"
             />
             <input
               className="form__field"
               type="password"
               value={password}
-              onChange={event => getPassword(event.target.value)}
+              onChange={(event) => getPassword(event.target.value)}
               placeholder="Password*"
             />
             <input
               className="form__field"
               type="password"
               value={validationPassword}
-              onChange={event => getValidationPassword(event.target.value)}
+              onChange={(event) => getValidationPassword(event.target.value)}
               placeholder="Re-enter Password*"
             />
             <div>{newUserError}</div>
