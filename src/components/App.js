5b24d16c605bb325643f4e438d2bd3508e9bda8c
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-11-17 15:47:16 +0000

    removed Content and ContentContainer, made App class

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import '../../styles/components/App.scss';
-import ContentContainer from '../containers/ContentContainer';
+import PropTypes from 'prop-types';
+import BalancesContainer from '../containers/BalancesContainer';
+import NewRoundContainer from '../containers/NewRoundContainer';
+import LoginContainer from '../containers/LoginContainer';
+import TabsContainer from '../containers/TabsContainer';
 
-function App() {
-  return (
-    <React.Fragment>
-      <ContentContainer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    const { store } = this.context;
+    const reduxState = store.getState();
+
+    this.state = {
+      stage: reduxState.stage,
+    };
+  }
+
+  componentDidMount() {
+    const { store } = this.context;
+    store.subscribe(() => {
+      this.updateFromStore();
+    });
+  }
+
+  updateFromStore() {
+    const { store } = this.context;
+    const reduxState = store.getState();
+    this.setState({
+      stage: reduxState.stage,
+    });
+  }
+
+  render() {
+    const { stage } = this.state;
+
+    return (
+      <div className="app">
+        {stage === 'login' && <LoginContainer />}
+        {stage === 'balances' && <BalancesContainer />}
+        {stage === 'newRound' && <NewRoundContainer />}
+        {stage === 'tabs' && <TabsContainer />}
+      </div>
+    );
+  }
 }
+
+App.contextTypes = {
+  store: PropTypes.object,
+};
 
 export default App;
